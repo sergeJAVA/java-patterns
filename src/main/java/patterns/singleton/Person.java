1a5e2a4a56a36cb735562d889a770381ef312d6c
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-06-24 11:59:56 +0300

    chore(singleton-pattern): удалил .idea и target из отслеживания и добавил в .gitignore, добавил больше javadoc, вместо инициализации переменной в статическом блоке сделал инициализацию напрямую.

--- src/main/java/patterns/singleton/Person.java
+++ src/main/java/patterns/singleton/Person.java
@@ -1,36 +1,61 @@
 package patterns.singleton;
 
+/**
+ * Класс Person представляет собой человека с именем и возрастом.
+ */
 public final class Person {
 
-    private static final Person INSTANCE;
+    private static final Person INSTANCE = new Person("Sergey", 20);
     private String name;
     private int age;
 
+    /**
+     * Конструктор класса Person (приватный, потому что класс является синглтоном).
+     * @param name Имя человека.
+     * @param age Возраст человека.
+     */
     private Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    static {
-        INSTANCE = new Person("Sergey", 20);
-    }
 
+    /**
+     * Возвращает экземпляр класса Person.
+     * @return Экземпляр класса Person.
+     */
     public static Person getInstance() {
         return INSTANCE;
     }
 
+    /**
+     * Возращает имя человека
+     * @return Имя человека.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Устанавливает имя человека.
+     * @param name Имя человека.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Возвращает возраст человека.
+     * @return Возраст человека.
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Устанавливает возраст человека.
+     * @param age Возраст человека.
+     */
     public void setAge(int age) {
         this.age = age;
     }
@@ -45,7 +70,7 @@ public final class Person {
 
     /**
      * Метод say выводить выводит в консоль сообщение, которое вы передаёте параметром message.
-     * @param message - сообщение для вывода в консоль.
+     * @param message сообщение для вывода в консоль.
      */
     public void say(String message) {
         System.out.println(message);
