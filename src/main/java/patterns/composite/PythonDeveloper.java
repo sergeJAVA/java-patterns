b1cd272a1e77a523c9f05866b251da73b36cca03
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-06-24 18:01:23 +0300

    feat(composite-pattern) Добавлена реализация паттерна Composite.

---
+++ src/main/java/patterns/composite/PythonDeveloper.java
@@ -0,0 +1,13 @@
+package patterns.composite;
+
+/**
+ * Класс {@code PythonDeveloper} является еще одним "листом" в паттерне Composite.
+ * Он реализует интерфейс {@code Developer} и представляет собой
+ * индивидуального Python-разработчика.
+ */
+public class PythonDeveloper implements Developer {
+    @Override
+    public void writeCode() {
+        System.out.println("Python developer writes code...");
+    }
+}
