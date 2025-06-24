8aafe0464f61f34d149ab9a6c533654d69df35c1
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-06-24 17:14:58 +0300

    feat(builder-pattern) Добавлена реализация паттерна Builder.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import patterns.builder.Pizza;
 import patterns.singleton.Person;
 
 public final class Main {
@@ -8,6 +9,15 @@ public final class Main {
 
     public static void main(String[] args) {
         Person.getInstance().say("Calling the “say()” method from the Person singleton class");
+
+        Pizza veggieDelight = new Pizza.Builder("Маленькая", "Gluten-Free Корочка")
+                .withSauce("Песто соус")
+                .addTopping("Грибы")
+                .addTopping("Лук")
+                .addTopping("Болгарский перец")
+                .withExtraCheese(false)
+                .build();
+        System.out.println("\n" + veggieDelight);
     }
 
 }
